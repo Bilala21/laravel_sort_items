6f860dedcb151746a81b4b302e26ac3ca024c85d
Bilala21 <bilal.kalri@gmail.com>
2022-04-21 09:57:19 +0500

    This app is about to sort items

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('order','ASC')->get();
+
+        return view('post',compact('posts'));
+    }
+
+    public function update(Request $request)
+    {
+        return response()->json(["msg" => $request]);
+        $posts = Post::all();
+
+        foreach ($posts as $post) {
+            foreach ($request->order as $order) {
+                if ($order['id'] == $post->id) {
+                    $post->update(['order' => $order['position']]);
+                }
+            }
+        }
+        
+        return response('Update Successfully.', 200);
+    }
+}
